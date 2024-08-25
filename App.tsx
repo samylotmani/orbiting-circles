5374de4f68ba22c7d319f63c0dec13df053a9cfe
Samy LOTMANI <lotmanisamy10@gmail.com>
2024-08-25 21:06:58 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MarqueeDemo } from './components/magicui/MarqueeDemo';
+
+const App = () => {
+  return (
+    <div>
+      <MarqueeDemo />
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      extend: {
-        animation: {
-          orbit: "orbit calc(var(--duration)*1s) linear infinite",
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
         },
-        keyframes: {
-          orbit: {
-            "0%": {
-              transform:
-                "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
-            },
-            "100%": {
-              transform:
-                "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
-            },
-          },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
         },
       },
     },
-  };
-  
+  },
+};
